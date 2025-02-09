0dce878e3b777e25e8a61d874dc7a61c957e4db5
Asim221202 <rahimasim15@gmail.com>
2025-02-09 16:51:46 +0400

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const GuildSettingsSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
-    partnerChannelId: { type: String, default: null },
-    partnerLogChannelId: { type: String, default: null }
+    partnerChannelId: { type: String, required: false },
+    partnerMessage: { type: String, required: false }, // Partner mesajı saklanıyor
 });
 
 module.exports = mongoose.model('GuildSettings', GuildSettingsSchema);
